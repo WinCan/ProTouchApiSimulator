c621928609bd8c778d7239b9e10df47a209e9f7f
Ivan <van9konst@users.noreply.github.com>
2022-07-15 19:18:37 +0200

    Update deploy.sh

--- deployment_linux/deploy.sh
+++ deployment_linux/deploy.sh
@@ -1,4 +1,4 @@
 exe_path=${1:-./build/bin/ProTouchApiSimulator}
-qmake_path=${3:-$(which qmake)}
+qmake_path=${2:-$(which qmake)}
 repo_path=$(dirname "$0")/../
-./linuxdeployqt $exe_path -unsupported-allow-new-glibc -verbose=2 -bundle-non-qt-libs -always-overwrite -qmldir=$repo_path -qmake=$qmake_path
+linuxdeployqt $exe_path -unsupported-allow-new-glibc -verbose=2 -bundle-non-qt-libs -always-overwrite -qmldir=$repo_path -qmake=$qmake_path
